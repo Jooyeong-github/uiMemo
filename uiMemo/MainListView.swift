d2bdaf644e7f322794f94b9dbf4c6eef4429738f
Jooyeong <supershicker@gmail.com>
2023-11-16 17:03:21 +0900

    step 1

---
+++ uiMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  uiMemo
+//
+//  Created by t2023-m0031 on 11/16/23.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
---
+++ uiMemo/Memory/Memo.swift
@@ -0,0 +1,8 @@
+//
+//  Memo.swift
+//  uiMemo
+//
+//  Created by t2023-m0031 on 11/16/23.
+//
+
+import Foundation
