9556d420de6b2265c17ee3c2f84b984f417c74bf
Jooyeong <supershicker@gmail.com>
2023-11-16 16:38:03 +0900

    Initial Commit

---
+++ uiMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uiMemo
+//
+//  Created by t2023-m0031 on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uiMemo/uiMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiMemoApp.swift
+//  uiMemo
+//
+//  Created by t2023-m0031 on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct uiMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
