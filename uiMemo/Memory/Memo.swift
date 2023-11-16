0df3e4db79a233f76764bf8e96d7b50da7c396fd
Jooyeong <supershicker@gmail.com>
2023-11-16 17:03:51 +0900

    step 2

--- uiMemo/Memory/Memo.swift
+++ uiMemo/Memory/Memo.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+class Memo : Identifiable, ObservableObject{
+    let id: UUID
+    @Published var content: String
+    
+    let insertData : Date
+    
+    init(content: String, insertData: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertData = insertData
+    }
+}
